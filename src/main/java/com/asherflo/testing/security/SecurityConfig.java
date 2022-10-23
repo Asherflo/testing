c2e092ea32849c14de6ed34725406e4f5a3c52da
Asherflo <aniyikayetemitopeflorence@gmail.com>
2022-10-23 09:28:15 +0100

    ...

--- src/main/java/com/asherflo/testing/security/SecurityConfig.java
+++ src/main/java/com/asherflo/testing/security/SecurityConfig.java
@@ -33,13 +33,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter  customAuthenticationFilter  = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
          http.csrf().disable();
          http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         http.authorizeRequests().antMatchers("api/login/**").permitAll();
          http.authorizeRequests().antMatchers(GET,"api/user/**").hasAnyAuthority("ROLE_USER");
          http.authorizeRequests().antMatchers(POST,"api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+         http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
 
-          http.authorizeRequests().anyRequest().authenticated();
-         http.addFilter( new CustomAuthenticationFilter(authenticationManagerBean()));
     }
 
     @Bean
